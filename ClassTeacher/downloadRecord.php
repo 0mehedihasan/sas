86f410f30a44e01c53f53b87dd4bcd79aae45dc1
0mehedihasan <mdmehedihasansr@gmail.com>
2024-11-02 13:54:13 +0600

    all works now ,only multi db not,sas_six working

--- ClassTeacher/downloadRecord.php
+++ ClassTeacher/downloadRecord.php
@@ -1,70 +1,72 @@
 <?php 
 error_reporting(0);
+session_start();
 include '../Includes/dbcon.php';
 include '../Includes/session.php';
 
-?>
-        <table border="1">
-        <thead>
-            <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Other Name</th>
-            <th>Admission No</th>
-            <th>Class</th>
-            <th>Class Arm</th>
-            <th>Session</th>
-            <th>Term</th>
-            <th>Status</th>
-            <th>Date</th>
-            </tr>
-        </thead>
+// Define the database connection variables
+$host = 'localhost:5222';
+$user = 'root';
+$pass = '';
 
-<?php 
-$filename="Attendance list";
+// Define the databases
+$dbs = ['sas_six', 'sas_seven', 'sas_eight', 'sas_other'];
+
+// Define the database connections
+$conn = [];
+foreach ($dbs as $db) {
+    $conn[$db] = new mysqli($host, $user, $pass, $db);
+    if ($conn[$db]->connect_error) {
+        die("Connection failed: " . $conn[$db]->connect_error);
+    }
+}
+
+$filename = "Attendance list";
 $dateTaken = date("Y-m-d");
 
-$cnt=1;			
-$ret = mysqli_query($conn,"SELECT tblattendance.Id,tblattendance.status,tblattendance.dateTimeTaken,tblclass.className,
-        tblclassarms.classArmName,tblsessionterm.sessionName,tblsessionterm.termId,tblterm.termName,
-        tblstudents.firstName,tblstudents.lastName,tblstudents.otherName,tblstudents.admissionNumber
+$cnt = 1;
+$ret = $conn['sas_six']->query("SELECT tblattendance.Id, tblattendance.status, tblattendance.dateTimeTaken, tblclass.className,
+        tblstudents.firstName, tblstudents.lastName, tblstudents.otherName, tblstudents.admissionNumber
         FROM tblattendance
         INNER JOIN tblclass ON tblclass.Id = tblattendance.classId
-        INNER JOIN tblclassarms ON tblclassarms.Id = tblattendance.classArmId
-        INNER JOIN tblsessionterm ON tblsessionterm.Id = tblattendance.sessionTermId
-        INNER JOIN tblterm ON tblterm.Id = tblsessionterm.termId
         INNER JOIN tblstudents ON tblstudents.admissionNumber = tblattendance.admissionNo
-        where tblattendance.dateTimeTaken = '$dateTaken' and tblattendance.classId = '$_SESSION[classId]' and tblattendance.classArmId = '$_SESSION[classArmId]'");
+        WHERE tblattendance.dateTimeTaken = '$dateTaken' AND tblattendance.classId = '$_SESSION[classId]'");
 
-if(mysqli_num_rows($ret) > 0 )
-{
-while ($row=mysqli_fetch_array($ret)) 
-{ 
-    
-    if($row['status'] == '1'){$status = "Present"; $colour="#00FF00";}else{$status = "Absent";$colour="#FF0000";}
+if ($ret->num_rows > 0) {
+        header("Content-type: application/octet-stream");
+        header("Content-Disposition: attachment; filename=".$filename."-report.xls");
+        header("Pragma: no-cache");
+        header("Expires: 0");
 
-echo '  
-<tr>  
-<td>'.$cnt.'</td> 
-<td>'.$firstName= $row['firstName'].'</td> 
-<td>'.$lastName= $row['lastName'].'</td> 
-<td>'.$otherName= $row['otherName'].'</td> 
-<td>'.$admissionNumber= $row['admissionNumber'].'</td> 
-<td>'.$className= $row['className'].'</td> 
-<td>'.$classArmName=$row['classArmName'].'</td>	
-<td>'.$sessionName=$row['sessionName'].'</td>	 
-<td>'.$termName=$row['termName'].'</td>	
-<td>'.$status=$status.'</td>	 	
-<td>'.$dateTimeTaken=$row['dateTimeTaken'].'</td>	 					
-</tr>  
-';
-header("Content-type: application/octet-stream");
-header("Content-Disposition: attachment; filename=".$filename."-report.xls");
-header("Pragma: no-cache");
-header("Expires: 0");
-			$cnt++;
-			}
-	}
+        echo '<table border="1">
+                <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Other Name</th>
+                        <th>Admission No</th>
+                        <th>Class</th>
+                        <th>Status</th>
+                        <th>Date</th>
+                        </tr>
+                </thead>';
+
+        while ($row = $ret->fetch_assoc()) {
+                $status = $row['status'] == '1' ? "Present" : "Absent";
+
+                echo '<tr>
+                        <td>'.$cnt.'</td>
+                        <td>'.$row['firstName'].'</td>
+                        <td>'.$row['lastName'].'</td>
+                        <td>'.$row['otherName'].'</td>
+                        <td>'.$row['admissionNumber'].'</td>
+                        <td>'.$row['className'].'</td>
+                        <td>'.$status.'</td>
+                        <td>'.$row['dateTimeTaken'].'</td>
+                </tr>';
+                $cnt++;
+        }
+        echo '</table>';
+}
 ?>
-</table>
