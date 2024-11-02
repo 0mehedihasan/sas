d219716eb24c613e6482668216e997ac6d2060dd
0mehedihasan <mdmehedihasansr@gmail.com>
2024-11-03 00:30:02 +0600

    view student attendance removed

--- ClassTeacher/downloadRecord.php
+++ ClassTeacher/downloadRecord.php
@@ -21,52 +21,76 @@ foreach ($dbs as $db) {
     }
 }
 
-$filename = "Attendance list";
-$dateTaken = date("Y-m-d");
+$statusMsg = ""; // Initialize the status message variable
 
-$cnt = 1;
-$ret = $conn['sas_six']->query("SELECT tblattendance.Id, tblattendance.status, tblattendance.dateTimeTaken, tblclass.className,
-        tblstudents.firstName, tblstudents.lastName, tblstudents.otherName, tblstudents.admissionNumber
-        FROM tblattendance
-        INNER JOIN tblclass ON tblclass.Id = tblattendance.classId
-        INNER JOIN tblstudents ON tblstudents.admissionNumber = tblattendance.admissionNo
-        WHERE tblattendance.dateTimeTaken = '$dateTaken' AND tblattendance.classId = '$_SESSION[classId]'");
+// Fetch class name for the class teacher from multiple databases
+$rrw = ['className' => ''];
+$classId = null;
+foreach ($dbs as $dbKey) {
+  $query = "SELECT tblclass.className, tblclassteacher.classId 
+            FROM tblclassteacher
+            INNER JOIN tblclass ON tblclass.Id = tblclassteacher.classId
+            WHERE tblclassteacher.Id = '$_SESSION[userId]'";
+  $rs = $conn[$dbKey]->query($query);
+  if ($rs && $rs->num_rows > 0) {
+    $rrw = $rs->fetch_assoc();
+    $classId = $rrw['classId'];
+    break;
+  }
+}
 
-if ($ret->num_rows > 0) {
-        header("Content-type: application/octet-stream");
-        header("Content-Disposition: attachment; filename=".$filename."-report.xls");
-        header("Pragma: no-cache");
-        header("Expires: 0");
+if ($classId) {
+    $filename = "Attendance list";
+    $dateTaken = date("Y-m-d");
 
-        echo '<table border="1">
-                <thead>
-                        <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Other Name</th>
-                        <th>Admission No</th>
-                        <th>Class</th>
-                        <th>Status</th>
-                        <th>Date</th>
-                        </tr>
-                </thead>';
+    $cnt = 1;
+    $ret = $conn['sas_six']->query("SELECT tblattendance.Id, tblattendance.status, tblattendance.dateTimeTaken, tblclass.className,
+            tblstudents.firstName, tblstudents.lastName, tblstudents.otherName, tblstudents.admissionNumber
+            FROM tblattendance
+            INNER JOIN tblclass ON tblclass.Id = tblattendance.classId
+            INNER JOIN tblstudents ON tblstudents.admissionNumber = tblattendance.admissionNo
+            WHERE tblattendance.dateTimeTaken = '$dateTaken' AND tblattendance.classId = '$classId'");
 
-        while ($row = $ret->fetch_assoc()) {
-                $status = $row['status'] == '1' ? "Present" : "Absent";
+    if ($ret->num_rows > 0) {
+            header("Content-type: application/octet-stream");
+            header("Content-Disposition: attachment; filename=".$filename."-report.xls");
+            header("Pragma: no-cache");
+            header("Expires: 0");
 
-                echo '<tr>
-                        <td>'.$cnt.'</td>
-                        <td>'.$row['firstName'].'</td>
-                        <td>'.$row['lastName'].'</td>
-                        <td>'.$row['otherName'].'</td>
-                        <td>'.$row['admissionNumber'].'</td>
-                        <td>'.$row['className'].'</td>
-                        <td>'.$status.'</td>
-                        <td>'.$row['dateTimeTaken'].'</td>
-                </tr>';
-                $cnt++;
-        }
-        echo '</table>';
+            echo '<table border="1">
+                    <thead>
+                            <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Other Name</th>
+                            <th>Admission No</th>
+                            <th>Class</th>
+                            <th>Status</th>
+                            <th>Date</th>
+                            </tr>
+                    </thead>';
+
+            while ($row = $ret->fetch_assoc()) {
+                    $status = $row['status'] == '1' ? "Present" : "Absent";
+
+                    echo '<tr>
+                            <td>'.$cnt.'</td>
+                            <td>'.$row['firstName'].'</td>
+                            <td>'.$row['lastName'].'</td>
+                            <td>'.$row['otherName'].'</td>
+                            <td>'.$row['admissionNumber'].'</td>
+                            <td>'.$row['className'].'</td>
+                            <td>'.$status.'</td>
+                            <td>'.$row['dateTimeTaken'].'</td>
+                    </tr>';
+                    $cnt++;
+            }
+            echo '</table>';
+    } else {
+        echo "No records found for the selected date.";
+    }
+} else {
+    echo "Class ID not found.";
 }
 ?>
